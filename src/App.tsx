e24577429fff606178ac01760b41ce1b0dd5e86d
Jörg Weissbecker <joergweissbecker@web.de>
2022-05-07 21:36:54 +0200

    + main pages shows overall costs

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { PowerupSelectionFactory } from './powerup-selection-factory';
 
 function App() {
+  const name = 'Josh Perez';
+  const powerup= PowerupSelectionFactory.create()
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Overall Costs are {powerup.overallCost()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
